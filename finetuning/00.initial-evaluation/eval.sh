92d77039c3a99d59c801e189020299f46345f7fb
Hanna Tuerk <hanna.tuerk@hotmail.de>
2025-06-12 11:49:33 +0200

    added documentation and saving of parity plots

--- finetuning/00.initial-evaluation/eval.sh
+++ finetuning/00.initial-evaluation/eval.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # Converting the model to a TorchScript format
+echo 'Exporting PET-MAD model from checkpoint'
 mtt export ../shared/models/pet-mad-v1.0.1.ckpt -o pet-mad-v1.0.1.pt
+
 # Evaluating the model on the test set and saving predictions to a file
+echo 'Now running Li3PS4 dataset'
 mtt eval pet-mad-v1.0.1.pt eval-options-Li3PS4.yaml -o Li3PS4-sample-test-predictions.xyz -b 8
+echo 'Now running MAD'
 mtt eval pet-mad-v1.0.1.pt eval-options-MAD.yaml -o MAD-sample-test-predictions.xyz -b 8
--- finetuning/01.full-finetuning/clean.sh
+++ finetuning/01.full-finetuning/clean.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-rm -rf $SCRIPT_DIR/outputs $SCRIPT_DIR/*.ckpt $SCRIPT_DIR/*.pt $SCRIPT_DIR/error.log $SCRIPT_DIR/*.xyz
+rm -rf $SCRIPT_DIR/outputs $SCRIPT_DIR/*.ckpt $SCRIPT_DIR/*.pt $SCRIPT_DIR/error.log $SCRIPT_DIR/*.xyz $SCRIPT_DIR/*.png
